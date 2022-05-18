494303242b1d726a16cfd84bb8bc3f00bed2d47c
Cyprien <cypthao@gmail.com>
2022-05-18 23:02:54 +0200

    feat: implement auth routes & UI (login/register) +

---
+++ client/src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <NTabs>
+        <NTabPane label="Tab 1">
+            <div>Tab 1 content</div>
+        </NTabPane>
+
+        <router-view />
+    </NTabs>
+</template>
+
+<script setup lang="ts"></script>
+
+<route lang="yaml">
+name: home
+meta:
+    auth: true
+</route>
