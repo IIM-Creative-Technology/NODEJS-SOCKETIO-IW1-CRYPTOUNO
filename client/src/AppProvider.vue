2fad88a954f437ad60bcf990fb2fbbb1655c31ca
Cyprien <cypthao@gmail.com>
2022-05-18 12:22:26 +0200

    feat: Initialize game client

---
+++ client/src/AppProvider.vue
@@ -0,0 +1,17 @@
+<template>
+    <n-config-provider>
+        <n-dialog-provider>
+            <n-message-provider>
+                <n-notification-provider>
+                    <n-loading-bar-provider>
+                        <slot />
+                    </n-loading-bar-provider>
+                </n-notification-provider>
+            </n-message-provider>
+        </n-dialog-provider>
+    </n-config-provider>
+</template>
+
+<script setup lang="ts">
+    import { NConfigProvider, NMessageProvider, NDialogProvider, NNotificationProvider, NLoadingBarProvider } from "naive-ui";
+</script>
---
+++ client/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import NaiveUI from "naive-ui";
+import "virtual:windi.css";
+
+createApp(App).use(NaiveUI).mount("#app");
