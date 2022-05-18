8c6d13a10fa9a5c9b2c25d692f2ed818040e0de9
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-05-19 01:18:45 +0200

    feat: set basic features of player service

--- server/src/modules/player/player.service.ts
+++ server/src/modules/player/player.service.ts
@@ -1,4 +1,26 @@
+import { PlayerStatus } from '@common/types/player.type';
+import { PlayerDocument, Player } from '@schemas/player.schema';
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class PlayerService {}
+export class PlayerService {
+  constructor(
+    @InjectModel(Player.name) private playerModel: Model<PlayerDocument>,
+  ) {}
+
+  findOne(id: string): Promise<PlayerDocument> {
+    return this.playerModel.findOne({ _id: id }).exec();
+  }
+
+  setPlayerStatus(id: string, status: PlayerStatus): Promise<PlayerDocument> {
+    return this.playerModel
+      .findOneAndUpdate({ _id: id }, { status }, { new: true })
+      .exec();
+  }
+
+  getOnlinePlayers(): Promise<PlayerDocument[]> {
+    return this.playerModel.find({ status: PlayerStatus.ONLINE }).exec();
+  }
+}
