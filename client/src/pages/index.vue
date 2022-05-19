774151e622bfc323b44ff514473a11c3e59bd665
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-05-19 04:17:28 +0200

    feat: Set-up dummy player list

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,17 +1,35 @@
 <template>
-    <NTabs>
-        <NTabPane label="Tab 1">
-            <div>Tab 1 content</div>
-        </NTabPane>
-
-        <router-view />
-    </NTabs>
+    <div class="flex flex-col gap-4">
+        <NCard :segmented="{ content: true }">
+            <div class="flex items-center gap-2 uppercase text-2xl !font-medium">
+                <NIcon><mdi:users /></NIcon>
+                <span>Players lobby</span>
+            </div>
+        </NCard>
+        <PlayerList :players="players" />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { ref } from "vue";
+    import PlayerList from "~/components/features/player/PlayerList.vue";
+
+    const players = ref<any[]>(
+        Array.from({ length: 20 }).map((_, i) => ({
+            _id: `${i}`,
+            username: `Player ${i}`,
+            gameId: i % 2 === 0 ? undefined : `${i}`,
+        })),
+    );
+
+    const shuffle = () => {
+        players.value.sort(() => Math.random() - 0.5);
+    };
+</script>
 
 <route lang="yaml">
 name: home
 meta:
-    auth: true
+    auth: false
+    layout: MainLayout
 </route>
