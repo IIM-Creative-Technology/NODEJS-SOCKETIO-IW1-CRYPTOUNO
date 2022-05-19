7a097c6fd64a4ee27222c63fedb87abe6cda608b
Cyprien <cypthao@gmail.com>
2022-05-19 14:50:09 +0200

    feat: implement TS definitions game / player

--- client/src/types/game.type.ts
+++ client/src/types/game.type.ts
@@ -1,8 +1,19 @@
 import { IPlayer } from "./player.type";
+import { ILobbyPlayer } from "~/types/player.type";
 
 export enum GameRecordResult {
     Draw = "Draw",
     Win = "Win",
+}
+
+export interface IGameState {
+    _id: string;
+    gameBoard: number[];
+    gameTurnCount: number;
+    activePlayer: string;
+    winCoordinates: number[] | null;
+    canPlay: boolean;
+    winner: string | null;
 }
 
 export interface IGameRecord {
@@ -13,13 +24,20 @@ export interface IGameRecord {
     bet?: number;
 }
 
+export interface IGameInvitation {
+    _id: string;
+    requester: ILobbyPlayer;
+    requestee: ILobbyPlayer;
+}
+
 export enum GameListenEvents {
     PlayerOnline = "player-online",
     PlayerOffline = "player-offline",
     PlayerJoinedGame = "player-joined-game",
+    LoadLobby = "load-lobby",
     GameInvitation = "game-invitation",
     GameInvitationFailed = "game-invitation-failed",
-    AnswetGameInvitationDeclined = "answer-game-invitation-declined",
+    AnswerGameInvitationDeclined = "answer-game-invitation-declined",
     AnswerGameInvitationFailed = "answer-game-invitation-failed",
     GameStarted = "game-started",
     PutTokenToBoardFailed = "put-token-to-board-failed",
--- client/src/types/player.type.ts
+++ client/src/types/player.type.ts
@@ -2,7 +2,14 @@ import { IGameRecord } from "./game.type";
 
 export interface IPlayer {
     _id?: string;
-    pseudo: string;
+    username: string;
     walletToken?: string;
     gameRecords: IGameRecord[];
+}
+
+export interface ILobbyPlayer {
+    _id: string;
+    username: string;
+    gameId?: string | null;
+    playerNumber?: 1 | 2;
 }
