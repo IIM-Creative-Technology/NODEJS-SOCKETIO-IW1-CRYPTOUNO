620418800681d77bd12e4bb74e2a793ea07146ea
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-05-19 02:35:31 +0200

    feat: Define event list enums client (Listen & Emit)

--- client/src/types/game.type.ts
+++ client/src/types/game.type.ts
@@ -1,8 +1,35 @@
 import { IPlayer } from "./player.type";
 
+export enum GameRecordResult {
+    Draw = "Draw",
+    Win = "Win",
+}
+
 export interface IGameRecord {
     _id?: string;
     players: IPlayer[];
-    winner: IPlayer;
+    result: GameRecordResult;
+    winner?: IPlayer;
     bet?: number;
+}
+
+export enum GameListenEvents {
+    PlayerOnline = "player-online",
+    PlayerOffline = "player-offline",
+    PlayerJoinedGame = "player-joined-game",
+    GameInvitation = "game-invitation",
+    GameInvitationFailed = "game-invitation-failed",
+    AnswetGameInvitationDeclined = "answer-game-invitation-declined",
+    AnswerGameInvitationFailed = "answer-game-invitation-failed",
+    GameStarted = "game-started",
+    PutTokenToBoardFailed = "put-token-to-board-failed",
+    UpdateGameState = "update-game-state",
+    GameEnded = "game-ended",
+}
+
+export enum GameEmitEvents {
+    SendGameInvitation = "send-game-invitation",
+    AnswerGameInvitation = "answer-game-invitation",
+    PutTokenToBoard = "put-token-to-board",
+    ReturnToMatchMaking = "return-to-match-making",
 }
